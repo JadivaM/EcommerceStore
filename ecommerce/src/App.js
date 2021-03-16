8d145946584f289916f9011bf1178a78206d9c08
Jadivam <Jmont261@fiu.edu>
2021-03-16 17:36:10 -0400

    connect commerce js and api key to app

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+const [products, setProducts] = useState([])
+const fetchProducts = () => {
+  commerce.products.list().then((products) => {
+    setProducts(products);
+  }).catch((err) => {
+    console.log(err)
+  });
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+}
+
   return (
-    <div className="App">
+    <>
+    <div>
     </div>
+    </>
   );
 }
 
