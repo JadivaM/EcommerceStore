d8b09a248090ac574e9639c8320bddd638f5820c
Jadivam <Jmont261@fiu.edu>
2021-03-19 02:52:07 -0400

    add landing page with text and hero image

---
+++ ecommerce/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+        <div className="home-container">
+            <div className="home-hero-image">
+                <p className="home-hero-image--text">Find Everything You Need For Your Home</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
