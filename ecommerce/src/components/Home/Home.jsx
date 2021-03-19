3d6cbd671cfe0daf2f11536459f461a3749ae39b
Jadivam <Jmont261@fiu.edu>
2021-03-19 12:55:09 -0400

    change hero image text

--- ecommerce/src/components/Home/Home.jsx
+++ ecommerce/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
     return (
         <>
         <div className="home-container">
             <div className="home-hero-image">
-                <p className="home-hero-image--text">Find Everything You Need For Your Home</p>
+                <p className="home-hero-image--text">Find Essentials For Your Home</p>
                 <p className="home-hero-image--button">Shop now</p>
             </div>
         </div>
