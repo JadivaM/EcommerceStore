cf9e56f9f6bbb1050e32cf6665fc4a222dada447
Jadivam <Jmont261@fiu.edu>
2021-03-18 23:09:07 -0400

    connected api to render products

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,23 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import Product from './components/Products/Product';
+import { commerce } from './lib/commerce';
 
-function App() {
-const [products, setProducts] = useState([])
-const fetchProducts = () => {
-  commerce.products.list().then((products) => {
-    setProducts(products);
-  }).catch((err) => {
-    console.log(err)
-  });
+const App = () => {
+  const [products, setProducts] = useState([])
 
+  const fetchProducts = () => {
+    try {
+      commerce.products.list().then((res) => {
+        setProducts(res.data);
+        console.log(res.data);
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+  
   useEffect(() => {
     fetchProducts();
-  }, [])
-}
+  }, []);
+  
 
   return (
     <>
-    <div>
+    <div className="product-cards">
+    {products.map(product => (
+        <Product key={product.id} name={product.name} image={product.media.source} {...product} />
+      ))}
     </div>
     </>
   );
