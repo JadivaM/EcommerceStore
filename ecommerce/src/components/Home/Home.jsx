b8397a4bdd939c52e4d6f2c0e43a51f8a76e53e3
Jadivam <Jmont261@fiu.edu>
2021-03-19 12:00:31 -0400

    edit landing page picture and font color

--- ecommerce/src/components/Home/Home.jsx
+++ ecommerce/src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
         <div className="home-container">
             <div className="home-hero-image">
                 <p className="home-hero-image--text">Find Everything You Need For Your Home</p>
+                <p className="home-hero-image--button">Shop now</p>
             </div>
         </div>
         </>
