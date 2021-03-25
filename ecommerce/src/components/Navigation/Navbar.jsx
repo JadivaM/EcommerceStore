fb33ea1fb28360814f9d98bc327d7421e190ed8d
Jadivam <Jmont261@fiu.edu>
2021-03-25 17:33:22 -0400

    create template for side nav with categories

--- ecommerce/src/components/Navigation/Navbar.jsx
+++ ecommerce/src/components/Navigation/Navbar.jsx
@@ -1,18 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import NavOptions from './NavOptions';
 
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const [wid, setWid] = useState(false);
+
+  const closeSidenav = () => {
+    setWid(false)
+  }
+
+  const openSidenav = ( ) => {
+      setWid(!wid)
+   }
 
     return (
         <>
+          {wid ? <NavOptions width={wid} closeNav={closeSidenav} /> : null}
         <div>
-        <AppBar position="static" style={{backgroundColor: '#fff'}}>
+        
+        <AppBar position="static" style={{backgroundColor: '#fff'}}> 
+        
         <Toolbar>
+      
          <IconButton edge="start" color="#000" aria-label="menu">
-            <MenuIcon />
+            <MenuIcon className="hamburger-icon" onClick={openSidenav} />
           </IconButton>
           <Typography variant="p" className="navbar-title">
             Handmade Studio
