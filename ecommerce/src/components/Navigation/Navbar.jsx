a573038686a8254d6ee3be0c2745a952cf91d005
Jadivam <Jmont261@fiu.edu>
2021-03-25 18:47:16 -0400

    add link to home on title

--- ecommerce/src/components/Navigation/Navbar.jsx
+++ ecommerce/src/components/Navigation/Navbar.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import {AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NavOptions from './NavOptions';
+import {Link} from 'react-router-dom';
 
-
-
-const Navbar = (props) => {
+const Navbar = () => {
   const [wid, setWid] = useState(false);
 
   const closeSidenav = () => {
@@ -20,17 +19,16 @@ const Navbar = (props) => {
         <>
           {wid ? <NavOptions width={wid} closeNav={closeSidenav} /> : null}
         <div>
-        
         <AppBar position="static" style={{backgroundColor: '#fff'}}> 
-        
         <Toolbar>
-      
          <IconButton edge="start" color="#000" aria-label="menu">
             <MenuIcon className="hamburger-icon" onClick={openSidenav} />
           </IconButton>
+          <Link to="/" style={{textDecoration: 'none'}}>
           <Typography variant="p" className="navbar-title">
             Handmade Studio
           </Typography>
+          </Link>
         </Toolbar>
       </AppBar>
             
