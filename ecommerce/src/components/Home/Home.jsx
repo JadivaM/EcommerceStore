853776a238bca12aab56e71ca6ad1b8bbdd18f6e
Jadivam <Jmont261@fiu.edu>
2021-03-25 18:46:56 -0400

    add navbar to home w link to home on title

--- ecommerce/src/components/Home/Home.jsx
+++ ecommerce/src/components/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import Navbar from '../Navigation/Navbar';
+import {Link}from 'react-router-dom';
 
 const Home = () => {
+
     return (
         <>
         <div className="home-container">
+          <Navbar />
             <div className="home-hero-image">
                 <p className="home-hero-image--text">Find Essentials For Your Home</p>
-                <p className="home-hero-image--button">Shop now</p>
+                <Link to='/products' style={{textDecoration: 'none'}}>
+                  <button className="home-hero-image--button">Shop now</button>
+                </Link>
+               
             </div>
         </div>
         </>
