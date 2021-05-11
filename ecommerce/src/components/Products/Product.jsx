8de7e07a5692a3fcf68dd92ba96eb38a30751ce9
Jadivam <Jmont261@fiu.edu>
2021-05-10 22:23:34 -0400

    add add to cart icon on cards

--- ecommerce/src/components/Products/Product.jsx
+++ ecommerce/src/components/Products/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Card, CardContent, CardMedia, Typography, StylesProvider } from '@material-ui/core';
-
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 const Product = (props) => {
     return (
@@ -9,13 +9,16 @@ const Product = (props) => {
         <StylesProvider injectFirst>
           <Card variant="outlined" style={{border: 'none'}}>
                 <CardMedia image={props.image}  title={props.name} />
-                <CardContent>
+                <CardContent className="product-info-container">
                     <Typography gutterBottom variant="p" component="p" className="product-card-text--title">
                         {props.name}
                     </Typography>
                     <Typography gutterBottom variant="p" component="p" className="product-card-text--price">
                         {props.price.formatted_with_symbol}
                     </Typography>
+                <div className="add-to-cart-container">
+                    <AddShoppingCartIcon  className="add-to-cart-icon" />
+                </div>
                 </CardContent>
             </Card>  
             </StylesProvider>
