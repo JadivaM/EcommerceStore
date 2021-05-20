b3f5321c0f5ce55605742e90a065fd55b486485f
JadivaM <jmont261@fiu.edu>
2021-05-20 15:53:03 -0400

    add page for specific category products

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Products from './components/Products/Products';
+import Category from './components/Categories/Category';
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/products" exact component={Products}/>
+        <Route path="/category/products/:id" exact component={Category}/>
       </Switch>
     </Router>
     </>
