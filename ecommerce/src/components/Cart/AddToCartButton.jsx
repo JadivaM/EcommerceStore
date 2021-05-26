5a4f949146778351066bc35d9407008b00295191
JadivaM <jmont261@fiu.edu>
2021-05-25 23:05:33 -0400

    add react toastify alerts after add items to cart

--- ecommerce/src/components/Cart/AddToCartButton.jsx
+++ ecommerce/src/components/Cart/AddToCartButton.jsx
@@ -1,22 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import { commerce } from '../../lib/commerce';
 import Button from '@material-ui/core/Button';
-import Alert from '@material-ui/lab/Alert';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddToCartButton = (props, {quantity}) => {
-const [addToCart, setAddToCart] = useState(null);
-const productId = `${props.id}`;
-const qty = (quantity);
-console.log(qty)
+const [addToCart, setAddToCart] = useState([]);
+
 
 const handleAddToCart = () => {
+    const productId = `${props.id}`;
+    const qty = (quantity);
+    console.log(qty);
     try {
         commerce.cart.add(productId, qty).then((res) => {
             setAddToCart(res); 
-            console.log(res);
+            console.log(addToCart)
             })
+            if (addToCart?.success === true) {
+            toast.success('Item added to cart!') 
+            }
     } catch(err) {
-        console.log(err);
+        toast.error('Something went wrong!')
+        console.log("Error", err);
     }
 }
 
@@ -25,6 +31,17 @@ const handleAddToCart = () => {
         <Button variant="contained" color="primary" onClick={handleAddToCart}>
         Add to cart
         </Button>
+        <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        />
         </div>
     )
 }
