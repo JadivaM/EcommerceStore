4935b13a43c91a8b82c61f77f0d810de4988975b
JadivaM <jmont261@fiu.edu>
2021-05-27 23:44:22 -0400

    style cart total component in shopping cart

--- ecommerce/src/components/Cart/CartTotal.jsx
+++ ecommerce/src/components/Cart/CartTotal.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 const CartTotal = (props) => {
     return (
-        <div>
-            <p>Total: {props.subtotal}</p>
+        <div className="cart-total-container">
+            <Paper elevation={3} className="cart-paper">
             <p>{props.items} items</p>
+            <p>Total: {props.subtotal}</p>
             <Button variant="contained" color="primary" disableElevation>Checkout</Button>
+            </Paper>
         </div>
     )
 }
 
-export default CartTotal
+export default CartTotal;
