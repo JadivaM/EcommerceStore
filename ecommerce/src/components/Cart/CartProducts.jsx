c81637c11f90b4eee9800b9f4c255aaf7c324a1b
JadivaM <jmont261@fiu.edu>
2021-05-29 15:38:47 -0400

    add remove cart items and update quantity

--- ecommerce/src/components/Cart/CartProducts.jsx
+++ ecommerce/src/components/Cart/CartProducts.jsx
@@ -1,22 +1,69 @@
 import React from 'react';
-import Divider from '@material-ui/core/Divider';
+import {Divider, IconButton} from '@material-ui/core';
+import ClearIcon from '@material-ui/icons/Clear';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import { commerce } from '../../lib/commerce';
 
 const CartProducts = (props) => {
+
+    const handleRemoveItem = () => {
+        try {
+            commerce.cart.remove(`${props.id}`).then(json => console.log(json));
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const addQuantity = () => {
+        try {
+        commerce.cart.update(`${props.id}`, { quantity: (Number(`${props.quantity}`) + 1) }).then((res) => {
+            console.log(res)
+            })
+    } catch (err) {
+        console.log(err)
+    }
+    }
+
+    
+     
+    
+       
+
+
+       
+            const subtractQuantity = () => {
+                try{
+                commerce.cart.update(`${props.id}`, { quantity: (Number(`${props.quantity}`) - 1) }).then((res) => {
+                    console.log(res)
+                    })
+            } catch (err) {
+                console.log(err)
+            }
+             }
+    
 
     return (
         <>
         <div className="shopping-cart-products-main-container">
         <Divider id="shopping-cart-item-divider"/> 
         <div className="shopping-cart-items-container">
-            <img style={{width: 200, height: 200, objectFit: 'cover'}}src={props.image} alt={props.name}/>
+            <img style={{width: 150, height: 150, objectFit: 'cover'}}src={props.image} alt={props.name}/>
             <div className="shopping-cart-items-column">
             <p>{props.name}</p>
             </div>
             <div className="shopping-cart-items-column">
-            <p>{props.quantity}</p>
+            <IconButton><RemoveIcon onClick={subtractQuantity} /> </IconButton>
+            <p>Quantity: {props.quantity}</p>
+            <IconButton><AddIcon onClick={addQuantity} /></IconButton>
             </div>
             <div className="shopping-cart-items-column">
             <p>{props.price}</p>
+            </div>
+            <div className="shopping-cart-items-button-column">
+            <IconButton>
+            <ClearIcon onClick={handleRemoveItem} />
+            </IconButton>
             </div>
         </div> 
         </div>
