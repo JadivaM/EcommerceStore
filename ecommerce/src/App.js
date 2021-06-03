6c0775cd83dfdbab4be13550de5961fa9e456a10
JadivaM <jmont261@fiu.edu>
2021-06-02 23:46:22 -0400

    add searchResult state and pass prop to components

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
@@ -7,18 +7,24 @@ import Products from './components/Products/Products';
 import Category from './components/Categories/Category';
 import ProductPage from './components/Products/ProductPage';
 import ShoppingCart from './components/Cart/ShoppingCart';
+import SearchResults from './components/Search/SearchResults';
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <>
     <Router>
-      <Navbar />
+      <Navbar setSearchResults={setSearchResults} />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/products" exact component={Products}/>
         <Route path="/category/products/:id" exact component={Category}/>
         <Route path="/product/:id" exact component={ProductPage}/>
         <Route path="/cart" exact component={ShoppingCart}/>
+        <Route path="/results" exact>
+          <SearchResults searchResults={searchResults} />
+        </Route>
       </Switch>
     </Router>
     </>
