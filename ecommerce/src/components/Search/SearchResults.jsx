533412293c76d590b06c9088f52517b2149869d2
JadivaM <jmont261@fiu.edu>
2021-06-03 00:31:38 -0400

    add ternary for when no search results are found

--- ecommerce/src/components/Search/SearchResults.jsx
+++ ecommerce/src/components/Search/SearchResults.jsx
@@ -7,11 +7,10 @@ const SearchResults = ({searchResults}) => {
     return (
         <>
         <div className="search-results-container">
-        <p className="products-category-results">{searchResults?.length} results</p>
+            {!searchResults || searchResults.length == 0 ? <p>No search Results found</p> :  <p className="products-category-results">{searchResults?.length} results</p>}
         </div>
         <div className="category-product-cards">
-         {searchResults?.map((product) => (
-
+         {searchResults && searchResults?.map((product) => (
              <ProductCard key={product.id} name={product.name} image={product.media.source} id={product.id} {...product} />
          ))}
          </div>
