e3fa851b14d438d94c580ff1232894217ff98521
JadivaM <jmont261@fiu.edu>
2021-06-02 22:04:35 -0400

    add navbar component to app.js

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Navbar from './components/Navigation/Navbar'
 import Products from './components/Products/Products';
 import Category from './components/Categories/Category';
 import ProductPage from './components/Products/ProductPage';
@@ -11,6 +12,7 @@ const App = () => {
   return (
     <>
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/products" exact component={Products}/>
