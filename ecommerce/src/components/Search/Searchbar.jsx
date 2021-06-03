f1726433977977db800139d7eebc2faa7be72508
JadivaM <jmont261@fiu.edu>
2021-06-02 23:46:58 -0400

    set search results querying API

--- ecommerce/src/components/Search/Searchbar.jsx
+++ ecommerce/src/components/Search/Searchbar.jsx
@@ -2,16 +2,14 @@ import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 
-const Searchbar = () => {
+const Searchbar = ({setSearchResults}) => {
     const [search, setSearch] = useState()
-    const [searchResults, setSearchResults] = useState([]);
     const history = useHistory();
 
       const handleSubmit = (e) => {
         e.preventDefault();
         commerce.products.list({'query': `${search}`}).then(response => {
             setSearchResults(response.data);
-            console.log(searchResults);
             history.push('/results');
         });
       }
