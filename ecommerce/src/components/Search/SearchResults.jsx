00bf1519b003a99d1a569e9fe58a7958a7faf9d5
JadivaM <jmont261@fiu.edu>
2021-06-03 23:35:11 -0400

    add ternary for search results rendering info

--- ecommerce/src/components/Search/SearchResults.jsx
+++ ecommerce/src/components/Search/SearchResults.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ProductCard from '../Products/ProductCard';
 
 const SearchResults = ({searchResults}) => {
-    console.log(searchResults)
-
     return (
         <>
         <div className="search-results-container">
-            {!searchResults || searchResults.length == 0 ? <p>No search Results found</p> :  <p className="products-category-results">{searchResults?.length} results</p>}
+            {!searchResults || searchResults.length === 0 ? <p>No search Results found</p> :  <p className="products-category-results">{searchResults?.length} results</p>}
         </div>
         <div className="category-product-cards">
          {searchResults && searchResults?.map((product) => (
