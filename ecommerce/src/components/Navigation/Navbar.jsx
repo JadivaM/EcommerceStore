9566ca32fc2667cc348a2e244dbcd7a8c4be6cc6
JadivaM <jmont261@fiu.edu>
2021-06-08 23:58:38 -0400

    add discounts to navbar using carousel

--- ecommerce/src/components/Navigation/Navbar.jsx
+++ ecommerce/src/components/Navigation/Navbar.jsx
@@ -1,35 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import {AppBar, Toolbar, Typography, IconButton, Badge} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
 import NavOptions from './NavOptions';
 import {Link} from 'react-router-dom';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Searchbar from '../Search/Searchbar';
+import DiscountsGrid from '../Discounts/DiscountsGrid';
+
+
+
 
 const Navbar = ({setSearchResults, totalItems}) => {
-  const [wid, setWid] = useState(false);
 
   useEffect(() => {
     console.log(totalItems)
   }, [totalItems])
 
-  const closeSidenav = () => {
-    setWid(false)
-  }
-
-  const openSidenav = ( ) => {
-      setWid(!wid)
-   }
-
     return (
-        <>
-          {wid ? <NavOptions width={wid} closeNav={closeSidenav} /> : null}
+        <> 
         <div>
         <AppBar position="static" style={{backgroundColor: '#fff'}}> 
         <Toolbar className="navbar">
-         <IconButton className="hamburger-icon-container" edge="start" color="#000" aria-label="menu">
-            <MenuIcon className="hamburger-icon" onClick={openSidenav} />
-          </IconButton>
           <div className="navbar-searchbar"><Searchbar setSearchResults={setSearchResults} /></div>
          
           <div className="navbar-title-container">
@@ -41,13 +31,21 @@ const Navbar = ({setSearchResults, totalItems}) => {
           </div>
          
           <Link to={'/cart'} style={{textDecoration: 'none'}}>
-          <IconButton style={{position: 'absolute', right: 0, top: 0, marginTop: 10, color: 'rgba(0, 0, 0, 0.8)'}}>
+          <IconButton style={{position: 'absolute', right: 0, top: 0, marginTop: 25, color: 'rgba(0, 0, 0, 0.8)'}}>
           <Badge badgeContent={totalItems} color="primary">
           <ShoppingCartIcon />
           </Badge>
           </IconButton>
           </Link>
+          <div className="category-options-container"> <NavOptions className="category-options" /></div>
         </Toolbar>
+       <div className="discounts-carousel-container">
+
+
+<DiscountsGrid className="discounts-carousel" />
+
+
+       </div>
       </AppBar>
             
         </div>
@@ -55,4 +53,4 @@ const Navbar = ({setSearchResults, totalItems}) => {
     )
 }
 
-export default Navbar
+export default Navbar;
