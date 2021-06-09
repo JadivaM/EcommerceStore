78e90ddae963be3d751f3a8f86ef3987d6ce74d2
JadivaM <jmont261@fiu.edu>
2021-06-08 23:57:25 -0400

    add handle updating product quantity state in cart

--- ecommerce/src/components/Cart/CartProducts.jsx
+++ ecommerce/src/components/Cart/CartProducts.jsx
@@ -1,68 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Divider, IconButton} from '@material-ui/core';
 import ClearIcon from '@material-ui/icons/Clear';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
+import Button from '@material-ui/core/Button';
 import { commerce } from '../../lib/commerce';
 
-const CartProducts = (props) => {
+const CartProducts = ({ item, setQuantity, onRemove}) => {
+    const [updateQuantity, setUpdateQuantity] = useState(item.quantity);
 
-    const handleRemoveItem = () => {
-        try {
-            commerce.cart.remove(`${props.id}`).then(json => console.log(json));
-        } catch (err) {
+    const handleRemoveItem = (itemId) => onRemove(itemId);
+
+    const handleQuantityChange = (e) => {
+        try{
+            setUpdateQuantity(e.target.value);
+            console.log(e.target.value);
+        } catch(err) {
             console.log(err)
         }
-    }
+      };
 
-    const addQuantity = () => {
-        try {
-        commerce.cart.update(`${props.id}`, { quantity: (Number(`${props.quantity}`) + 1) }).then((res) => {
-            console.log(res)
-            })
-    } catch (err) {
-        console.log(err)
-    }
-    }
-
-    
-     
-    
-       
-
-
-       
-            const subtractQuantity = () => {
-                try{
-                commerce.cart.update(`${props.id}`, { quantity: (Number(`${props.quantity}`) - 1) }).then((res) => {
-                    console.log(res)
-                    })
-            } catch (err) {
-                console.log(err)
-            }
-             }
-    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        commerce.cart.update(item.id, { quantity: `${updateQuantity}` }).then(response => {
+            setQuantity(response.quantity);
+        });
+        };
 
     return (
         <>
         <div className="shopping-cart-products-main-container">
         <Divider id="shopping-cart-item-divider"/> 
         <div className="shopping-cart-items-container">
-            <img style={{width: 150, height: 150, objectFit: 'cover'}}src={props.image} alt={props.name}/>
+            <img style={{width: 150, height: 150, objectFit: 'cover'}} src={item.media.source} alt={item.name}/>
             <div className="shopping-cart-items-column">
-            <p>{props.name}</p>
+            <p>{item.name}</p>
             </div>
             <div className="shopping-cart-items-column">
-            <IconButton><RemoveIcon onClick={subtractQuantity} /> </IconButton>
-            <p>Quantity: {props.quantity}</p>
-            <IconButton><AddIcon onClick={addQuantity} /></IconButton>
+            <p>Quantity:</p>
+            <form onSubmit={handleSubmit}>
+            <input type="text" onChange={handleQuantityChange} defaultValue={item.quantity} />
+            <Button type="submit">Update</Button>
+            </form>
             </div>
             <div className="shopping-cart-items-column">
-            <p>{props.price}</p>
+            <p>{item.price.formatted_with_symbol}</p>
             </div>
             <div className="shopping-cart-items-button-column">
             <IconButton>
-            <ClearIcon onClick={handleRemoveItem} />
+            <ClearIcon onClick={() => handleRemoveItem(item.id)} />
             </IconButton>
             </div>
         </div> 
