d0343214ee30a7be70d5c129bc98bcf23e9d55e2
JadivaM <jmont261@fiu.edu>
2021-08-07 23:46:31 -0400

    add step labels for material ui steppers and corresponding logic for order of each form component to be shows with each step and add handle next and back page functionality

--- ecommerce/src/components/Checkout/CheckoutFormPage.jsx
+++ ecommerce/src/components/Checkout/CheckoutFormPage.jsx
@@ -28,68 +28,58 @@ const CheckoutFormPage = ({cartItem, setCartItem, setCartTotal}) => {
     
           generateToken();
         }
-      }, []);
+      }, [cartItem]);
 
 
 
     const getSteps = () => {
-        return ['Shipping information'];
+        return ['Shipping information', 'Payment Details', 'Review and Order'];
       }
 
       function getStepContent(step) {
         switch (step) {
           case 0:
             return (<ShippingInformationForm checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} />);
-          // case 1:
-          //   return (<PaymentDetailsForm checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} />);
-          // case 1:
-          //   return <ReviewCheckoutInformation checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} cartItem={cartItem} />;
-          // default:
-          //   return (<ShippingInformationForm checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} />);
+          case 1:
+            return (<PaymentDetailsForm checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} />);
+          case 2:
+            return (<ReviewCheckoutInformation checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} cartItem={cartItem} />);
+          default:
+            return (<ShippingInformationForm checkoutToken={checkoutToken} setFormData={setFormData} formData={formData} />);
         }
       }
 
       
     const steps = getSteps();  
-    const maxSteps = steps.length;
 
     const handleNext = () => {
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      commerce.cart.empty().then((response) => {
-      setCartItem(response.cart)
-      setCartTotal(response.cart.totalItems)});
     };
 
-    // const handleBack = () => {
-    //   setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    // };
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
     return (
         <div className="checkout-stepper-container">
-             <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map(label => (
-          <Step key={label}>
+          <Stepper activeStep={activeStep} alternativeLabel>
+            {steps.map(label => (
+            <Step key={label}>
             <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      <div>
-        {activeStep === steps.length ? (
-          <div style={{textAlign: 'center'}}>
-           Your order is confirmed.
-          </div>
-        ) : (
-          <div  className="checkout-step">
+            </Step>
+            ))}
+          </Stepper>
+        <div>
+        <div className="checkout-step">
             {getStepContent(activeStep)}<div>
-              {/* <Button disabled={activeStep === 0} onClick={handleBack}>
-                Back
-              </Button> */}
-              <Button variant="contained" color="primary" onClick={handleNext} type="submit">
-                {activeStep === steps.length - 1 ? "Submit" : null }
-              </Button>
-            </div>
-          </div>
-        )}
+            <Button disabled={activeStep === 0} onClick={handleBack}>
+              Back
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleNext} type="submit">
+              {activeStep === steps.length - 1 ? "Submit" : "Next" }
+            </Button>
+        </div>
+        </div>
       </div>
     </div>
     )
