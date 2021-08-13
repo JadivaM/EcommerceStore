63bf4d0a241e673c030b2c75f4e0a1c8b28df3f2
JadivaM <jmont261@fiu.edu>
2021-08-13 13:27:19 -0400

    make textfields required in the shipping information form and add buttons to move back and forward in the stepper steps and remove state for user state as it is not needed as it is part of formData and will be saved that way

--- ecommerce/src/components/Checkout/ShippingInformationForm.jsx
+++ ecommerce/src/components/Checkout/ShippingInformationForm.jsx
@@ -5,13 +5,13 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
-const CheckoutForm = ({formData, setFormData, checkoutToken}) => {
+const CheckoutForm = ({formData, setFormData, checkoutToken, handleBack, handleNext}) => {
     const [states, setStates] = useState([]);
-    const [userState, setUserState] = useState('');
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+      setFormData({ ...formData, [e.target.name]: e.target.value });
       };
 
       const fetchStates = () => {
@@ -22,37 +22,36 @@ const CheckoutForm = ({formData, setFormData, checkoutToken}) => {
 
 
   useEffect(() => {
-    fetchStates(checkoutToken?.id, userState);
-  }, [userState]);
+    fetchStates();
+  }, [checkoutToken]);
 
     return (
         <div>
             <div>
             <FormControl id="form-margin">
-            <TextField className="checkout-form-row" id="outlined-basic" label="First name" variant="outlined" value={formData.firstName} onChange={handleChange} name="firstName" />
+            <TextField className="checkout-form-row" label="First name" variant="outlined" value={formData.firstName || ""} onChange={handleChange} required name="firstName" />
             </FormControl>
             <FormControl id="form-margin">
-            <TextField className="checkout-form-row" id="outlined-basic" label="Last name" variant="outlined" value={formData.lastName} onChange={handleChange} name="lastName" />
+            <TextField className="checkout-form-row" label="Last name" variant="outlined" value={formData.lastName || ""} onChange={handleChange} required name="lastName" />
             </FormControl>
             </div>
             <div>
             <FormControl fullWidth id="form-margin">
-            <TextField id="outlined-basic" label="Email" variant="outlined" value={formData.email} onChange={handleChange} name="email" />
+            <TextField label="Email" required variant="outlined" value={formData.email || ""} onChange={handleChange} name="email" />
             </FormControl>
             </div>
             <div>
             <FormControl fullWidth id="form-margin">
-           <TextField id="outlined-basic" label="Address" variant="outlined" value={formData.address} onChange={handleChange} name="address"  />
+           <TextField label="Address" required variant="outlined" value={formData.address || ""}  onChange={handleChange} name="address"  />
            </FormControl>
            </div>
             <div>
-           
             <FormControl id="form-margin">
-            <TextField id="outlined-basic" label="City" variant="outlined" value={formData.city} onChange={handleChange} name="city"  />
+            <TextField label="City" required variant="outlined" value={formData.city || ""} onChange={handleChange} name="city"  />
             </FormControl>
-            <FormControl style={{width: 100}} id="form-margin" variant="outlined">
+            <FormControl required style={{width: 100}} id="form-margin" variant="outlined">
             <InputLabel>State</InputLabel>
-            <Select name="state" value={formData.state} onChange={handleChange}>
+            <Select name="state" defaultValue="" onChange={handleChange}>
                 {Object.entries(states).map(([code, name]) => ({ id: code, label: name })).map((item) => (
                   <MenuItem key={item.id} value={item.id}>
                     {item.label}
@@ -61,7 +60,15 @@ const CheckoutForm = ({formData, setFormData, checkoutToken}) => {
             </Select>
             </FormControl>
             <FormControl style={{width: 140}} id="form-margin">
-            <TextField id="outlined-basic" label="Zip code" variant="outlined" value={formData.zip} onChange={handleChange} name="zip" />
+            <TextField required label="Zip code" variant="outlined" value={formData.zip || ""} onChange={handleChange} name="zip" />
+            <div className="stepper-buttons-container">
+            <Button onClick={handleBack}>
+              Back
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleNext} type="submit">
+              Next
+            </Button>
+            </div>
             </FormControl>
             </div>
             
