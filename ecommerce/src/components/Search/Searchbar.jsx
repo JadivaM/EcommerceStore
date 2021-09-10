18d5dded2f755e1e24e9ab13533129fad5aea42a
JadivaM <jmont261@fiu.edu>
2021-09-10 13:42:23 -0400

    remove e.target.reset() due to error as I am using state for the input in the search bar and instead added an icon for users to reset the input if needed

--- ecommerce/src/components/Search/Searchbar.jsx
+++ ecommerce/src/components/Search/Searchbar.jsx
@@ -2,11 +2,12 @@ import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { commerce } from '../../lib/commerce';
 import SearchIcon from '@material-ui/icons/Search';
+import CancelIcon from '@material-ui/icons/Cancel';
 import { AppContext } from '../../context/AppContext';
 
 const Searchbar = ({setSearchResults}) => {
     const { setLoading } = useContext(AppContext);
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState('')
     const history = useHistory();
 
       const handleSubmit = (e) => {
@@ -16,7 +17,6 @@ const Searchbar = ({setSearchResults}) => {
             setLoading(false);
             setSearchResults(response.data);
             history.push('/results');
-            e.target.reset();
         });
       }
  
@@ -24,7 +24,14 @@ const Searchbar = ({setSearchResults}) => {
         <div>
             <form className="search-bar" onSubmit={handleSubmit}>
             <input placeholder="search" className="search-bar-input" type="text" onChange={(e) => setSearch(e.target.value)} />
-            <SearchIcon onClick={handleSubmit} className="search-bar-icon"/>
+            <span className="search-bar-icons-container">
+              <button className="search-bar-icon-button" type="reset">
+                <CancelIcon id="search-bar-icon"/>
+              </button>
+              <button className="search-bar-icon-button" type="submit">
+                <SearchIcon id="search-bar-icon"/>
+              </button>
+            </span>
             </form>
         </div> 
     )
